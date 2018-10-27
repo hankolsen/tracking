3ae6b37cf33290f4395f69c685dbecbbe29e18e7
hank <hank.delang@enfogroup.com>
2018-10-27 16:49:07 +0200

    Try responding with image data

--- lambda-src/collect.js
+++ lambda-src/collect.js
@@ -54,10 +54,9 @@ exports.handler = (event, context, callback) => {
   callback(null, {
     headers: {
       'Content-Type': 'image/gif',
-      'Content-Length': 35,
       'Cache-Control': 'no-cache, no-store, must-revalidate',
     },
     statusCode: 200,
-    body: '',
+    body: 'GIF89a�����,D;',
   });
 };
